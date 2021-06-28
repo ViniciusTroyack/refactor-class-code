8378955bce14814794575267f50a3da2a4ad8e54
willian <willian@pop-os.localdomain>
2021-06-27 21:50:34 -0300

    chore: activity refactor class code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import DisplayWeather from "./components/DisplayWeather";
+import SearchWeather from "./components/SearchWeather";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    weather: {},
+    city: "",
+  };
+
+  handleChangeWeather = (weatherData, city) => {
+    this.setState({ weather: weatherData, city: city });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <SearchWeather handleChangeWeather={this.handleChangeWeather} />
+        <DisplayWeather info={this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
